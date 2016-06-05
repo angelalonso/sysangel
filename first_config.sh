b9fe593f1bd7d327515aa822d74cf27d104744df
angelalonso <alonsofonseca.angel@gmail.com>
2016-06-05 08:36:39 +0200

    dev nulling everything

--- first_config.sh
+++ first_config.sh
@@ -1,22 +1,29 @@
 #!/bin/usr/env bash
 #
 # Script to configure a new machine to use sysangel
+set -eo pipefail
 
 INSTALLDIR="$HOME/sysangel"
 PROFILEDIR="/etc/profile.d"
 
 main() {
   echo "- Installing work directory..."
-  mkdir -p ${INSTALLDIR}
+  mkdir -p ${INSTALLDIR} &> /dev/null
   echo "  DONE"
 
   echo "- Installing Profile.d script..."
-  curl -o /etc/profile.d/sysangel.sh https://raw.githubusercontent.com/angelalonso/sysangel/master/profile_sysangel.sh
+  curl -o /etc/profile.d/sysangel.sh \
+    https://raw.githubusercontent.com/angelalonso/sysangel/master/profile_sysangel.sh \
+    &> /dev/null
   echo "  DONE"
 
-  echo " - Installing ${INSTALLDIR} main python script..."
-  curl -o ${INSTALLDIR}/sysangel.py https://raw.githubusercontent.com/angelalonso/sysangel/master/sysangel.py
+  echo "- Installing ${INSTALLDIR} main python script..."
+  curl -o ${INSTALLDIR}/sysangel.py \
+    https://raw.githubusercontent.com/angelalonso/sysangel/master/sysangel.py \
+    &> /dev/null
   echo "  DONE"
+
+  echo "- Please, indicate name for this machine"
 
 }
 
