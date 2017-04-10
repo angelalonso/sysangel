98d134c59610d12ddc7cb3e2725796dedc358b80
angelalonso <alonsofonseca.angel@gmail.com>
2017-04-10 22:03:49 +0200

    first try at keybindings

--- scripts/xfce_autostart.sh
+++ scripts/xfce_autostart.sh
@@ -3,3 +3,16 @@
 
 # Check mounted Priv does not delay use of ssh
 /home/aaf/sysangel/scripts/priv_data.sh
+
+# Compose keys -> RCTRL + (n ~) = ñ
+setxkbmap  -option compose:rctrl
+xmodmap ~/.Xmodmap
+
+
+xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
+
+
+## Detect and configure touchpad. See 'man synclient' for more info.
+if egrep -iq 'touchpad' /proc/bus/input/devices; then
+    synclient VertEdgeScroll=1 TapButton1=1 &
+fi
