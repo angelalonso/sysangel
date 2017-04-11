19338e7479202d12044e340bd238a207b24015df
Angel Alonso <a.fonseca@foodpanda.com>
2017-04-11 13:50:52 +0200

    adding brewed packages

--- macinstall.sh
+++ macinstall.sh
@@ -25,7 +25,7 @@ echo "TASK: COPY OVER SSH KEYS, GPG KEYS"
 xcode-select --install
 
 # reinstall python manually
-brew install python3 python
+brew install python3 python tcptraceroute awscli base64 encfs docker git htop kubernetes-cli terraform zsh zsh-completions
 
 
 # vim config
