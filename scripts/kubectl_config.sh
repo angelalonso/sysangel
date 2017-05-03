f4c22ed7b53aff23f4a3ee513b1ba8a32e89c512
aaf <aaf@eeedeb>
2017-05-03 21:01:20 +0200

    script to install kubectl modified

--- scripts/kubectl_config.sh
+++ scripts/kubectl_config.sh
@@ -7,7 +7,7 @@ else
 fi
 
 echo $VERS
-# curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/386/kubectl
-#
-# chmod +x ./kubectl
-# sudo mv ./kubectl /usr/local/bin/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/$VERS/kubectl
+
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
