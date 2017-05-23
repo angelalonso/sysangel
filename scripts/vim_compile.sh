2b83af83e18b8b9151af4b679ac5bf2f424c99bf
aaf <aaf@eeedeb>
2017-05-23 22:20:40 +0200

    vim no longer needed to be compiled, kept around just in case

--- scripts/vim_compile.sh
+++ scripts/vim_compile.sh
@@ -7,35 +7,37 @@ HOME="/home/${USR}"
 GITDIR="${HOME}/sysangel"
 
 install(){
-  echo "compiling vim"
-  sudo apt-get install checkinstall libncurses5-dev \
-    libatk1.0-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
-
-  remove
-
-  cd ~
-  git clone https://github.com/vim/vim.git
-  cd vim
-  ./configure --with-features=huge \
-    --enable-multibyte \
-    --enable-rubyinterp=yes \
-    --enable-pythoninterp=yes \
-    --with-python-config-dir=/usr/lib/python2.7/config-i386-linux-gnu \
-    --enable-python3interp=yes \
-    --with-python3-config-dir=/usr/lib/python3.4/config-3.4m-i386-linux-gnu \
-    --enable-perlinterp=yes \
-    --enable-luainterp=yes \
-    --enable-gui=gtk2 --enable-cscope --prefix=/usr
-  make VIMRUNTIMEDIR=/usr/share/vim/vim80
-  cd ~/vim
-  sudo checkinstall
-
-  sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-  sudo update-alternatives --set editor /usr/bin/vim
-  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-  sudo update-alternatives --set vi /usr/bin/vim
+  echo "compiling vim no longer needed, using apt-get instead (v8.0 already)"
+#  sudo apt-get install checkinstall libncurses5-dev \
+#    libatk1.0-dev \
+#    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+#    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+#
+#  remove
+#
+#  cd ~
+#  git clone https://github.com/vim/vim.git
+#  cd vim
+#  ./configure --with-features=huge \
+#    --enable-multibyte \
+#    --enable-rubyinterp=yes \
+#    --enable-pythoninterp=yes \
+#    --with-python-config-dir=/usr/lib/python2.7/config-i386-linux-gnu \
+#    --enable-python3interp=yes \
+#    --with-python3-config-dir=/usr/lib/python3.4/config-3.4m-i386-linux-gnu \
+#    --enable-perlinterp=yes \
+#    --enable-luainterp=yes \
+#    --enable-gui=gtk2 --enable-cscope --prefix=/usr
+#  make VIMRUNTIMEDIR=/usr/share/vim/vim80
+#  cd ~/vim
+#  sudo checkinstall
+#
+#  sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+#  sudo update-alternatives --set editor /usr/bin/vim
+#  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+#  sudo update-alternatives --set vi /usr/bin/vim
+  sudo apt-get update
+  sudo apt-get install vim
 
   # Add vimbrant colorscheme
   mkdir -p ${HOME}/.vim/colors
