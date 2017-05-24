37676d536275594ebaad67ade3ef59b50c38b133
aaf <aaf@zen>
2017-05-24 08:39:16 +0200

    installation of Franz done

--- xfceinstall.sh
+++ xfceinstall.sh
@@ -181,6 +181,33 @@ otherpackages(){
 #sudo apt-get update
 #sudo apt-get install virtualbox-5.1
   # Franz
+# Taken from https://gist.github.com/ruebenramirez/22234da93f08be65125cc45fc386c1cd
+
+sudo rm -fr /opt/franz
+sudo rm -fr /usr/share/applications/franz.desktop
+
+
+# create installation dir
+sudo mkdir -p /opt/franz
+
+#install franz
+# 4.0.4 is end of may, latest
+wget -qO- https://github.com/meetfranz/franz-app/releases/download/4.0.4/Franz-linux-x64-4.0.4.tgz | sudo tar xvz -C /opt/franz/
+
+# add app icon
+sudo wget "https://cdn-images-1.medium.com/max/360/1*v86tTomtFZIdqzMNpvwIZw.png" -O /opt/franz/franz-icon.png
+
+# configure app for desktop use
+sudo bash -c "cat <<EOF > /usr/share/applications/franz.desktop
+[Desktop Entry]
+Name=Franz
+Comment=
+Exec=/opt/franz/Franz
+Icon=/opt/franz/franz-icon.png
+Terminal=false
+Type=Application
+Categories=Messaging,Internet
+EOF"
 echo "here"
 }
 
