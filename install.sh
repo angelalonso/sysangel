de4a83bf3000078cfd811ee28eb4916888dbd894
aaf <alonsofonseca.angel@gmail.com>
2017-07-16 14:29:22 +0200

    adding compizmanager for ubuntu

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ python python-pip python-pip seahorse sudo tcptraceroute terminator \
 unzip xbindkeys xvkbd zim zip zsh"
 
 DEB_PKGS="chromium"
-UBU_PKGS="google-chrome-stable indicator-multiload"
+UBU_PKGS="compizconfig-settings-manager google-chrome-stable indicator-multiload"
 
 # Bash colors
 BLU='\033[0;34m'
