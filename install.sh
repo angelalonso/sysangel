5c866aa589b0d38f2068ed4f974b49638fc9b24c
aaf <alonsofonseca.angel@gmail.com>
2017-07-18 18:13:42 +0200

    ppapurge for ubuntu

--- install.sh
+++ install.sh
@@ -23,7 +23,8 @@ python python-pip python-pip seahorse sudo tcptraceroute terminator \
 unzip xbindkeys xvkbd zim zip zsh"
 
 DEB_PKGS="chromium"
-UBU_PKGS="compizconfig-settings-manager gnome-tweak-tool google-chrome-stable indicator-multiload"
+UBU_PKGS="compizconfig-settings-manager gnome-tweak-tool google-chrome-stable \
+indicator-multiload ppa-purge"
 
 # Bash colors
 BLU='\033[0;34m'
