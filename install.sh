e5836a937fa3ede501beb8f0360c9b5bda804e5b
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2018-09-12 23:24:19 +0200

    new start with a new machine

--- install.sh
+++ install.sh
@@ -1,32 +1,88 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-#If a command fails, make the whole script exit
-#Treat unset variables as an error, and immediately exit.
-#Disable filename expansion (globbing) upon seeing *, ?, etc..
-#Produce a failure return code if any command errors
-set -euf -o pipefail
+#I had to 
+# partition from windows, take note of partitions and recovery keys and whatnot
+# burn iso with rufus (UEFI mode)
+# disable RAID on BIOS, then enable it again
 
-FLDR_MAIN=$(dirname $0)
-FLDR_SCRIPTS="$FLDR_MAIN/scripts"
+mkdir -p ~/Software/Dev
+mkdir -p ~/.ssh
+mkdir -p ~/.kube
 
 
-bash $FLDR_SCRIPTS/install_packages.sh
-echo "Packages installed"
+sudo apt-get update 
+sudo apt-get upgrade
+sudo apt-get install \
+curl \
+cryfs \
+exfat-fuse \
+exfat-utils \
+expect \
+fabric \
+gimp \
+git \
+git \
+htop \
+inkscape \
+iotop \
+jq \
+keepassx \
+mtr \
+nmap \
+openssh-client \
+passwd \
+pwgen \
+python \
+python-boto3 \
+python-pip \
+python3 \
+python3-pip \
+tcptraceroute \
+terminator \
+unzip \
+vim \
+vim-gtk \
+wine-stable \
+zip \
+zsh \
+zsh \
+cheese
 
-#-# Make dirs needed? Only if it does not belong anywhere else or it belongs in several scripts
+echo "OH MY ZSH"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-#-# Configure (programs, keys)
-# ohmyzsh
-# vim
-# firefox developer sync
-# thunderbird copy from HDD
-# terminator
-# fonts
-# caps2esc
-# awscli
-# kubectl
-# terraform
-# docker
-# helm
-# kops
-# privd
+echo "KUBECTL"
+echo " as in https://kubernetes.io/docs/tasks/tools/install-kubectl/"
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+echo "MINIKUBE"
+echo "https://github.com/kubernetes/minikube/releases"
+echo "or directly curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/"
+
+echo "FIREFOX"
+echo "https://www.mozilla.org/de/firefox/developer/"
+
+echo "VIRTUALBOX"
+echo "https://www.virtualbox.org/wiki/Linux_Downloads"
+echo "https://askubuntu.com/questions/760671/could-not-load-vboxdrv-after-upgrade-to-ubuntu-16-04-and-i-want-to-keep-secur/768310#768310"
+echo "sudo apt-get install dkms build-essential linux-headers-`uname -r`"
+
+echo "CONFIG:"
+echo " - firefox"
+echo " - keepassx"
+echo " - manually install fantasque font from sysangel repo, files"
+
+cd ~/Software/Dev
+git clone https://github.com/angelalonso/sysangel
+
+cd ~
+mv .zshrc .zshrc_orig
+ln -s ~/Software/Dev/sysangel/files/zshrc_home ~/.zshrc
+
+mv ~/.config/terminator/config ~/.config/terminator/config_orig
+ln -s ~/Software/Dev/sysangel/files/terminator_config ~/.config/terminator/config
