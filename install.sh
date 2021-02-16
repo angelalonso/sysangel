e7df530cd59b89f3fa41ae2eba6dfd96f0c57814
Angel Alonso <angel.fonseca@deliveryhero.com>
2021-02-16 08:14:39 +0100

    installing awscli

--- install.sh
+++ install.sh
@@ -38,5 +38,8 @@ zsh
 # docker
 # terraform
 # awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 #   pip install awscli --user
 #saml2aws
