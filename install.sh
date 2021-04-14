e8bb19f36e4c7ca501f4cd0e19c65a8cf71f5c21
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-04-14 10:59:04 +0200

    improving docs and configs

--- install.sh
+++ install.sh
@@ -218,6 +218,8 @@ install_scripts() {
 
 latest_considerations() {
   log_txt warn "REMEMBER TO NOW DO THE FOLLOWING:"
+  log_txt warnlista "SSH Keys, GPG, credentials..."
+  log_txt warnlistb "Copy them over from your backups"
   log_txt warnlista "Firefox"
   log_txt warnlistb "Import your GPG Keys to Mailvelope"
   log_txt warnlista "Rambox"
@@ -225,13 +227,39 @@ latest_considerations() {
   log_txt warnlista "Dropbox"
   log_txt warnlistb "Configure your accounts"
   log_txt warnlistb "Configure autolaunch"
+  log_txt warnlista "Gnome"
+  log_txt warnlistb "Set CTRL-ALT-W to open Webbrowser"
+  log_txt warnlistb "Set CTRL-ALT-F to open File browser"
+  log_txt warnlista ".zshrc"
+  log_txt warnlistb "aliases"
 }
 
+config_scripts() {
 
-create_dirs
-apt_install
-install_scripts
-# config_scripts
-#   vimrc
-#   terminator config
+# zshrc
+# TODO: check if current is a link and it's correct, then uncomment this:
+#mv $HOME/.zshrc $HOME/.zshrc.orig
+#ln -s $(pwd)/files/zshrc_home $HOME/.zshrc
+
+# Fonts
+# TODO: check if folder exists first
+#sudo mkdir -p /usr/share/fonts/truetype/fantasque
+# TODO: check if file exists first
+#sudo cp files/FantasqueSansMono_Regular.ttf /usr/share/fonts/truetype/fantasque/
+
+# Terminator
+#mkdir $HOME/.config/terminator
+# TODO: check if current is a link and it's correct, then uncomment this:
+#mv $HOME/.config/terminator/config $HOME/.config/terminator/config.orig
+#ln -s $(pwd)/files/terminator_config ~/.config/terminator/config
+
+# Vim
+# TODO: install with Plug
+#ln -s $(pwd)/files/vimrc $HOME/.vimrc
+}
+
+#create_dirs
+#apt_install
+#install_scripts
+config_scripts
 latest_considerations
