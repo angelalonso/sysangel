23b2bd2d6491b71c70e530d43010eedf82ab6b60
Angel Alonso <angel.fonseca@deliveryhero.com>
2022-10-14 12:16:26 +0200

    ssh-fridge, hopefully very useful

--- scripts/ssh-fridge.sh
+++ scripts/ssh-fridge.sh
@@ -6,7 +6,7 @@ SSHFRIDGEPATH="$SSHPATH/.fridge"
 GREENLIST="$SSHPATH/.list"
 
 
-function turn_on {
+function enable-keys {
   echo ON $1
   if [ "$1" == "" ]; then
     echo " Which key do you want to enable?:"
@@ -20,16 +20,18 @@ function turn_on {
     mv $SSHFILEPATH $SSHPATH/
     echo "moved $SSHFILEPATH out of the Fridge"
   fi
-
+  NEWSSHFILEPATH=$(echo $SSHFILEPATH | sed 's#\.fridge/##g')
+  ssh-add $NEWSSHFILEPATH
+  refresh-agent
 }
 
-function turn_off {
+function disable-all-keys {
   mkdir -p $SSHFRIDGEPATH
   LIST=$(grep KEY $SSHPATH/* 2>/dev/null | awk -F":" '{print $1}' | sort | uniq)
   for i in $LIST; do
     GREENED=False
     for j in $(cat $GREENLIST); do
-      if [[ "$i" == *"$j"* ]]; then
+      if [[ "$i" == "$SSHPATH/$j"* ]]; then
         echo "Not moving $i to the fridge!"
         GREENED=True
       fi
@@ -39,20 +41,54 @@ function turn_off {
       mv $i $SSHFRIDGEPATH/
     fi
   done
+  refresh-agent
+}
 
+function refresh-agent {
+  echo
+  echo "Loaded SSH Keys:"
+  ssh-add -l
+  echo
+  while true; do
+    read -p "Do you want to refresh the SSH Agent loaded keys? " yn
+    case $yn in
+      [Yy]* ) ssh-add -D;
+        LIST=$(grep KEY $SSHPATH/* 2>/dev/null | awk -F":" '{print $1}' | sort | uniq)
+        for i in $LIST; do
+          ssh-add $i
+        done
+        echo 
+        echo "Loaded SSH Keys:"
+        ssh-add -l
+        break;;
+      [Nn]* ) 
+        exit;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
+
+function modify-greenlist {
+  vim $GREENLIST
 }
 
 function help {
   echo SYNTAX:
-  echo " $0 [on|off]"
+  echo " $0 [out|in|greenlist]"
+  echo "   $0 out        - takes a key out of the fridge, ready to use"
+  echo "   $0 in         - puts all key on .ssh into the fridge, disabling them"
+  echo "   $0 greenlist  - Edits the Greenlist of keys that must not go into the fridge"
 }
 
 case $1 in
-  "on")
-    turn_on $2
+  "out")
+    enable-keys $2
     ;;
-  "off")
-    turn_off
+  "in")
+    disable-all-keys
+    ;;
+  "green"|"greenlist")
+    modify-greenlist
     ;;
   *)
     help
