bc08378cfce39257fe93d112c6a4658bf806ed12
Angel Alonso <alonsofonseca.angel@gmail.com>
2022-10-30 22:38:50 +0100

    menu for bash tools

---
+++ scripts/aa.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# We define functions and store them on a dictionary
+declare -A fn
+
+fn[alarm]=run_alarm
+function run_alarm {
+  ./alarm.sh
+}
+
+fn[ssh]=run_ssh
+function run_ssh {
+  echo "running ssh stuff"
+}
+
+function check_fn {
+  ${fn[$1]}
+}
+
+function menu {
+  chosen=$(for i in "${!fn[@]}"; do echo $i; done | fzf)
+  ${fn[$chosen]}
+}
+
+function help {
+ echo "SYNTAX: $0 <action>"
+ echo ""
+ echo "- Actions available:"
+ echo " - alarm"
+ echo "${!fn[@]}"
+}
+
+if [[ $1 == "" ]]; then
+  menu
+else
+  check_fn $1
+fi
+
+
--- scripts/alarm.sh
+++ scripts/alarm.sh
@@ -2,16 +2,12 @@
 clear
 
 if [[ $1 == "" ]]; then
-  echo "ERROR: Not a valid Alarm received!"
-  echo "  SYNTAX:"
-  echo "$0 <Alarm Time in HH:MM Format>"
-  echo "  E.g.: $0 $(date '+%H:%M')"
-  exit 2
+  read -p "Enter the time for the Alarm (E.g.: $(date '+%H:%M')) " ALARM
 else
   ALARM=$1
-
-  sleep $(( $(date --date="$ALARM" +%s) - $(date +%s) ));
-
-  echo -e "\e[41m\e[5mALARM!"
-  notify-send "ALARM at $ALARM"
 fi
+
+sleep $(( $(date --date="$ALARM" +%s) - $(date +%s) ));
+
+echo -e "\e[41m\e[5mALARM!"
+notify-send "ALARM at $ALARM"
