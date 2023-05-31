7498f55594cec364906cc0b50b0b612e5f7a4472
Angel Alonso <alonsofonseca.angel@gmail.com>
2023-05-31 22:55:03 +0200

    switcher of keyboard layouts Mac-Linux

---
+++ scripts/keyboards_mac_n_normal.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+switch_to_int () {
+  for id in $(xinput -list | grep 'AT Translated Set 2 keyboard' | cut -f 2 | cut -d= -f 2); do
+    echo Setting $id MAIN
+    # NOTE: I swap Left Alt and Left Win to make the layout mroe similar to Mac's
+    setxkbmap -device $id -layout de -variant nodeadkeys -option "altwin:swap_lalt_lwin" 
+      #setxkbmap -device $id -layout de -option altwin:swap_lalt_lwin -option lv3:lalt_switch
+      #setxkbmap -device $id -layout de -option altwin:swap_alt_win
+  done
+}
+
+switch_to_ext () {
+  for id in $(xinput -list | grep 'Angel Alonso' | cut -f 2 | cut -d= -f 2); do
+    echo Setting $id EXT
+    setxkbmap -device $id -layout us -variant mac
+  done
+}
+
+# NOTE: All this is because I cannot have two separate sets and work simultaneously (Eg: I switch Alt and Win for internal, and external has it swapped too)
+EXT=$(xinput -list | grep 'Angel Alonso' | cut -f 2 | cut -d= -f 2)
+if [[ "$EXT" == "" ]]; then
+  echo "no EXT connected"
+  switch_to_int
+else
+  echo "EXT connected is connected, do you want to switch to US Mac Keyboard Layout?"
+  while true; do
+      read -p "$* [y/n]: " yn
+      case $yn in
+          [Yy]*) switch_to_ext; exit 0 ;;  
+          [Nn]*) switch_to_int; exit 0 ;;
+      esac
+  done
+fi
