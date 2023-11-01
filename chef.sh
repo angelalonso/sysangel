e302d9541d9d5898e93fca906f4ea71081a5b41b
angelalonso <alonsofonseca.angel@gmail.com>
2023-11-01 22:37:11 +0100

    rust, python...work

--- chef.sh
+++ chef.sh
@@ -1,12 +1,46 @@
 #!/usr/bin/env bash
 
-#gem install chef-zero
-# TODO: COPY config.yaml FROM BACKUP - Private
-# TODO: Set a template config.yaml here
-# TODO: MOVE .deb to Private, copy over
-# TODO: MAYBE INSTALL ecryptfs first??
-sudo dpkg -i ./chef-workstation_23.4.1032-1_amd64.deb
-
-sudo chef-client --local-mode  ./chef/local.rb
+# TODO: 
+# - INSTALL ecryptfs first
+# - mount external disk
+# - get everything from Private
+# - 
 
 
+#sudo chef-client --local-mode  ./chef/local.rb
+
+get_config() {
+  echo " - Copying over Config.yaml"
+  echo " --------------------------"
+  cp ./config.yaml ./config.yaml.bck
+  cp ${CONFIG_PATH}/config.yaml .
+}
+
+restore_config() {
+  echo " - Restoring Config.yaml template"
+  echo " --------------------------------"
+  mv ./config.yaml.bck ./config.yaml
+}
+
+install_chef() {
+  echo " - Installing Chef Package"
+  echo " -------------------------"
+  cp ${CONFIG_PATH}/${CHEF_DEB} .
+  sudo dpkg -i ${CHEF_DEB}
+  rm ${CHEF_DEB}
+}
+
+run_chef() {
+  echo " - Running Chef"
+  echo " --------------"
+  sudo chef-client --local-mode  ./chef/local.rb
+}
+
+## VARIABLES
+CONFIG_PATH="${HOME}/Private"
+CHEF_DEB="chef-workstation_23.4.1032-1_amd64.deb"
+
+get_config
+#install_chef
+run_chef
+restore_config
--- chef/local.rb
+++ chef/local.rb
@@ -1,68 +1,106 @@
-#apt_update 'Update the apt cache' do action end
-#
-#directory 'test' do
-#  owner 'aaf'
-#  group 'aaf'
-#  mode '0755'
-#  action :create
-#end
-#
-#package %w(
-#apt-transport-https
-#arp-scan
-#bat
-#chromium-browser
-#cryfs
-#curl
-#exa
-#exfat-fuse
-#exfatprogs
-#expect
-#fabric
-#gimp
-#git 
-#gnupg2
-#htop
-#inkscape
-#iotop
-#jq
-#mtr
-#net-tools
-#nmap
-#openssh-client
-#passwd
-#pcsxr
-#pwgen
-#supertuxkart
-#tcptraceroute
-#terminator
-#unzip
-#vim 
-#vim-gtk
-#vlc
-#xsel
-#xterm
-#zip
-#zsh
-#) do
-#    action :install
-#end
 require 'yaml'
 pwd = File.expand_path File.dirname(__FILE__)
 conf = YAML.load(File.read("#{pwd}/../config.yaml"))
-puts "----------------------------"
-puts conf['CHEF_PCKGS']
-puts "----------------------------"
+homedir = "/home/#{conf['USER']}"
 
-#package %w(
-#) do
-#    action :install
-#end
-# variables locally (copy over with chef.sh)
-# Use those variables to create Folders
-# Secrets, keys...
-# oh my zsh https://supermarket.chef.io/cookbooks/lxmx_oh_my_zsh/versions/0.4.1
-# Rust
-# Python 3.11
+
+## APT PACKAGES
+##############
+
+# Update the package cache
+apt_update 'update' do
+  action :update
+end
+
+package (
+conf['CHEF_PCKGS']
+) do
+    action :install
+end
+
+
+## DIRECTORIES
+##############
+
+directory "#{homedir}/Software" do
+  owner conf['USER']
+  group conf['USER']
+  mode '0755'
+  action :create
+end
+directory "#{homedir}/Software/Dev" do
+  owner conf['USER']
+  group conf['USER']
+  mode '0755'
+  action :create
+end
+directory "#{homedir}/Software/Work" do
+  owner conf['USER']
+  group conf['USER']
+  mode '0755'
+  action :create
+end
+directory "#{homedir}/.ssh" do
+  owner conf['USER']
+  group conf['USER']
+  mode '0700'
+  action :create
+end
+
+## Oh My ZSH
+############
+execute 'ohmyzsh_install' do
+  command 'sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"'
+  not_if {::File.directory?("#{homedir}/.oh-my-zsh") }
+end
+
+## Rust
+############
+# Install rustup
+execute 'install rustup' do
+  command 'curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y'
+  user conf['USER']
+  environment ({ 'HOME' => "#{homedir}" }) # Set the home directory for the user
+  not_if {::File.file?("#{homedir}/.cargo/bin/rustup") }
+end
+# Add Rust to the PATH
+execute 'add rust to PATH' do
+  command 'echo \'export PATH="$HOME/.cargo/bin:$PATH"\' >> $HOME/.zshrc'
+  user conf['USER']
+  environment ({ 'HOME' => "#{homedir}" }) # Set the home directory for the user
+  not_if "grep -q \"HOME/.cargo/bin\" #{homedir}/.zshrc"
+end
+
+## Python3.11
+############
+# Install the prerequisites
+package %w(software-properties-common) do
+  action :install
+end
+# Add the deadsnakes PPA for Python 3.11
+apt_repository 'deadsnakes-ppa' do
+  uri 'ppa:deadsnakes/ppa'
+  action :add
+end
+# Install Python 3.11
+package 'python3.11' do
+  action :install
+end
+# Install pip for Python 3.11
+package 'python3-pip' do
+  action :install
+end
+# Update the alternatives to set Python 3.11 as the default
+execute "update-alternatives" do
+  command "update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1"
+  action :run
+end
+# Update the default Python version
+execute "update-alternatives-set" do
+  command "update-alternatives --set python3 /usr/bin/python3.11"
+  action :run
+end
+
+# Copy Secrets, keys...from mount volume on Chef.sh
 # wine
 # 
