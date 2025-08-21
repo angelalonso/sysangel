4af3e050d68bc802f49afa985920f5ca1dd34f55
Angel Alonso Fonseca <angel.alonso.fonseca@westernacher.com>
2025-08-21 12:13:16 +0200

    adding neovim

---
+++ cfgfiles/nvim/init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+
+-- Editing
+vim.opt.shiftwidth = 2
+
+-- Look & Feel
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+
---
+++ config.sh
@@ -0,0 +1,14 @@
+
+# # Neovim
+# # https://www.youtube.com/watch?v=TQn2hJeHQbM&list=PLep05UYkc6wTyBe7kPjQFWVXTlhKeQejM&index=1
+# # REQUIREMENTS
+# sudo apt install luarocks
+# # BUILD
+# git clone https://github.com/neovim/neovim
+# cd neovim
+# make CMAKE_BUILD_TYPE=RelWithDebInfo
+# sudo make install
+# mkdir -p ~/.config/nvim
+# # CONFIG
+# cp -R cfgfiles/nvim/* ~/.config/nvim/
+
